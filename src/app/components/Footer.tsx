74a4653d48db00119a53bbcb79097522f7f71f46
Ante Koceić <ante@crocoder.dev>
2026-02-12 12:06:15 +0100

    Updated translations

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,23 @@
+import { useTranslations } from "next-intl";
+
 const Footer = () => {
+  const t = useTranslations("Footer");
+  
   return (
     <footer className="w-full border-t bg-black px-6 py-8 text-sm text-white">
       <div className="m-auto flex w-full max-w-6xl flex-col gap-3">
         <div className="flex flex-row gap-1">
-          <div>Email: email@email.com</div>
-          <div>Phone: 123-4567</div>
-          <div>Address: 123 Example Street, City, Country</div>
+          <div className="flex flex-col">
+            <div>
+              {t('workingHours')}
+            </div>
+            <div>
+              {t("meetings")}
+            </div>
+          </div>
+          <div>Email: {t('emailAdress')}</div>
+          <div>Phone: {t('phoneNumber')}</div>
+          <div>Address: {t('address')}</div>
         </div>
       </div>
     </footer>
