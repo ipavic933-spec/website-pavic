4e8c326d5f8771bb62e0efb69486fa46f18ac4a7
Ivan Ivic <ivanivic842@gmail.com>
2026-02-13 17:17:24 +0100

    Feat: header and hero sections (#7)
    * feat: hero section component, new badge shadcn component, config import to globals.css
    
    * feat: create responsive header,fix translates and hero
    
    * fix: footer import
    
    * fix: copy and letter spacing

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,17 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 export default withNextIntl(nextConfig);
+
+
