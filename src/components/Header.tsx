8fb6a2daf567acfc140b7621197de3749987a342
vele616 <vele@crocoder.dev>
2026-02-20 13:43:03 +0100

    chore: header and footer color systematization

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -47,7 +47,7 @@ export function Header() {
   }, []);
 
   return (
-    <header className="bg-linear-to-b from-brand-900 to-brand-800 fixed top-0 left-0 right-0 z-50">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-brand-900">
       <div className="">
         <div className="mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
           <Logo hideText={true} />
