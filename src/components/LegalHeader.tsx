30c3ca976beb093bd7b2d5d84dd84b5ea26dfac4
Ante Koceić <ante@crocoder.dev>
2026-02-24 12:08:19 +0100

    Removed #top as default herf for Logo component

--- src/components/LegalHeader.tsx
+++ src/components/LegalHeader.tsx
@@ -4,12 +4,11 @@ import { LanguageSwitch } from "./LanguageSwitch";
 import { Logo } from "./Logo";
 
 export function LegalHeader() {
-
   return (
     <header className="bg-linear-to-b from-brand-900 to-brand-800 fixed top-0 left-0 right-0 z-50">
       <div className="mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
-        <Logo hideText={true} href="/"/>
-        {/* <LanguageSwitch /> */}
+        <Logo hideText={true} />
+        <LanguageSwitch />
       </div>
     </header>
   );
